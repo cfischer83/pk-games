28c3b6d505cc06c3f8fb912d3547dec950fafcb4
Cory Fischer <cfischer83@aol.com>
2021-10-30 00:45:17 -0700

    peninsula and water, obstacles without being hittable

--- mech/js/l1.js
+++ mech/js/l1.js
@@ -1,6 +1,13 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	var enemy1 = addEnemy("turret", 50, 1550, 850);
 	var enemy2 = addEnemy("turret", 50, 1550, 1150);
+	var enemy3 = addEnemy("turret", 50, 1500, 2800);
+	var enemy4 = addEnemy("turret", 50, 1800, 2800);
+	var enemy5 = addEnemy("turret", 50, 1400, 3100);
+	var enemy6 = addEnemy("turret", 50, 1700, 3000);
+	var enemy7 = addEnemy("turret", 50, 1400, 3400);
+	var enemy8 = addEnemy("turret", 50, 1700, 3250);
+	//var enemy9 = addEnemy("turret", 50, 1400, 2800);
 
 	var obstacle1 = addObstacle(831, 286, 1530, 0);
 	var obstacle2 = addObstacle(250, 150, 1300, 0);
@@ -10,6 +17,12 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var obstacle6 = addObstacle(173, 144, 318, 1159);
 	var obstacle7 = addObstacle(416, 286, 3367, 509);
 	var obstacle8 = addObstacle(561, 286, 3148, 675);
+	var obstacleWater1 = addObstacle(2999, 100, 0, 1700, false);
+	var obstacleWater2 = addObstacle(200, 500, 2799, 1700, false);
+	var obstacleWater3 = addObstacle(1260, 939, 1949, 2099, false);
+	var obstacleWater4 = addObstacle(1000, 700, 950, 2099, false);
+	var obstacleWater5 = addObstacle(400, 750, 899, 2775, false);
+	var obstacleWater6 = addObstacle(70, 100, 1300, 2775, false);
 });
 
 function addEnemy(enemyType, life, left, top, direction = "left") {
@@ -28,14 +41,14 @@ function addEnemy(enemyType, life, left, top, direction = "left") {
 	return enemy;
 }
 
-function addObstacle(width, height, left, top) {
+function addObstacle(width, height, left, top, hittable = true) {
 	var ground = document.getElementById("ground");
 	var obstacle = document.createElement("div");
 	var rando = Math.random();
 		obstacle.setAttribute("class", "obstacle");
 		obstacle.setAttribute("id", "obstacle" + rando);
 		obstacle.setAttribute("data-allegiance", "obstacle");
-		obstacle.setAttribute("data-hittable", "true");
+		obstacle.setAttribute("data-hittable", hittable);
 		ground.appendChild(obstacle);
 		obstacle.style.marginLeft = parseInt(left) + "px";
 		obstacle.style.marginTop = parseInt(top) + "px";
