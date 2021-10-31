3bffa36e34f4e07ba7f15c8c3adfaa2b5378a70d
Cory Fischer <cfischer83@aol.com>
2021-10-31 00:41:42 -0700

    level one adjustments, fixes to make character move on turn more quickly

--- mech/js/l1.js
+++ mech/js/l1.js
@@ -7,16 +7,18 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	var enemy6 = addEnemy("turret", 50, 1700, 3000);
 	var enemy7 = addEnemy("turret", 50, 1400, 3400);
 	var enemy8 = addEnemy("turret", 50, 1700, 3250);
-	//var enemy9 = addEnemy("turret", 50, 1400, 2800);
+	var enemy9 = addEnemy("turret", 50, 3282, 742);
+	var enemy10 = addEnemy("turret", 50, 3682, 503);
+	var enemy11 = addEnemy("turret", 50, 3583, 207);
 
 	var obstacle1 = addObstacle(831, 286, 1530, 0);
 	var obstacle2 = addObstacle(250, 150, 1300, 0);
-	var obstacle3 = addObstacle(390, 350, 2100, 250);
+	var obstacle3 = addObstacle(390, 350, 2099, 249);
 	var obstacle4 = addObstacle(190, 230, 1923, 230);
 	var obstacle5 = addObstacle(364, 230, 0, 1000);
 	var obstacle6 = addObstacle(173, 144, 318, 1159);
-	var obstacle7 = addObstacle(416, 286, 3367, 509);
-	var obstacle8 = addObstacle(561, 286, 3148, 675);
+	var obstacle7 = addObstacle(535, 286, 2921, 909);
+	var obstacle8 = addObstacle(561, 486, 2698, 1192);
 	var obstacleWater1 = addObstacle(2999, 100, 0, 1700, false);
 	var obstacleWater2 = addObstacle(200, 500, 2799, 1700, false);
 	var obstacleWater3 = addObstacle(1260, 939, 1949, 2099, false);
